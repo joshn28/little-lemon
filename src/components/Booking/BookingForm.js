79e886c69c5374efd48ecb67c02dbaf33765d3e4
Joshua Nobleza <joshnobleza@bell.net>
2023-11-15 18:24:06 -0500

    Convert to controlled component

--- src/components/Booking/BookingForm.js
+++ src/components/Booking/BookingForm.js
@@ -1,31 +1,48 @@
+import { useState } from 'react';
 import '../../styles/BookingPage.scss';
 
 function BookingForm() {
+    const [form, setForm] = useState({
+        date: '',
+        time: '17:00',
+        guests: 1,
+        occasion: 'Birthday',
+    });
+
+    const availableTimes = [
+        '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
+    ];
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(form)
+    }
+
+    function minDate() {
+        let today = new Date();
+        return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Reserve a table</h1>
             <div>
                 <label htmlFor="res-date">Choose a date</label>
-                <input type="date" id="res-date"/>
+                <input type="date" id="res-date" value={form.date} onChange={(e) => setForm({ ...form, date: e.target.value })} min={minDate()}/>
             </div>
             <div>
                 <label htmlFor="res-time">Choose time</label>
-                <select name="res-time" id="res-time">
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
+                <select name="res-time" id="res-time" value={form.time} onChange={(e) => setForm({ ...form, time: e.target.value })}>
+                    {availableTimes.map((time, i) => <option key={i}>{time}</option>)}
                 </select>
             </div>
             <div>
                 <label htmlFor="guests"># of guests</label>
-                <input type="number" placeholder="1" min="1" max="10" id="guests" />
+                <input type="number" placeholder="1" min="1" max="10" id="guests" value={form.guests} onChange={(e) => setForm({ ...form, guests: e.target.value })}/>
             </div>
             <div>
                 <label htmlFor="occasion">Occasion</label>
-                <select name="occasion" id="occasion">
+                <select name="occasion" id="occasion" value={form.occasion} onChange={(e) => setForm({ ...form, occasion: e.target.value })} >
                     <option>Birthday</option>
                     <option>Anniversary</option>
                 </select>
