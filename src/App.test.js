1ef6b411ee30958eefb6570eeb630b4e7dc089ec
Joshua Nobleza <joshnobleza@bell.net>
2023-11-16 18:58:10 -0500

    Add unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import BookingForm from './components/Booking/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the BookingForm heading', () => {
+    render(<BookingForm availableTimes={[]} />);
+
+    const headingElement = screen.getByText("Reserve a table");
+    expect(headingElement).toBeInTheDocument();
+});
+
+describe('initializeTimes', () => {
+  test('Populates the time select field from state', () => {
+    render(<BookingForm availableTimes={['17:00']} />);
+
+    const timePicker = screen.getByLabelText('Choose time');
+    expect(timePicker.childElementCount).toBe(1);
+  });
+});
+
+describe('updateTimes', () => {
+  test('Returns the same initial times', () => {
+    render(<BookingForm availableTimes={['17:00']} />);
+
+    const timePicker = screen.getByLabelText('Choose time');
+    const times = timePicker.childElementCount;
+
+    const datePicker = screen.getByLabelText('Choose a date');
+    fireEvent.change(datePicker, { target: { value: 'yyyy-mm-dd'} });
+    const updatedTimes = timePicker.childElementCount;
+
+    expect(times).toEqual(updatedTimes);
+  });
 });
