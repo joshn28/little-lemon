f688e6a682502ca146cd136c54444c8924cf83a8
Joshua Nobleza <joshnobleza@bell.net>
2023-11-22 17:38:44 -0500

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,26 +8,39 @@ test('Renders the BookingForm heading', () => {
     expect(headingElement).toBeInTheDocument();
 });
 
+test('Check if the time select and submit button are disabled', () => {
+  render(<BookingForm availableTimes={[]} />);
+
+  const timePicker = screen.getByLabelText('Choose a time');
+  expect(timePicker).toBeDisabled();
+
+  const submitButton = screen.getByText('Make your reservation').closest('button');
+  expect(submitButton).toBeDisabled();
+});
+
 describe('initializeTimes', () => {
   test('Populates the time select field from state', () => {
-    render(<BookingForm availableTimes={['17:00']} />);
+    const mockDispatch = jest.fn().mockReturnValueOnce([]);
+    render(<BookingForm availableTimes={['17:00']} dispatch={mockDispatch} />);
 
-    const timePicker = screen.getByLabelText('Choose time');
-    expect(timePicker.childElementCount).toBe(1);
+    const timePicker = screen.getByLabelText('Choose a time');
+    expect(timePicker.childElementCount).toBe(2);
   });
 });
 
 describe('updateTimes', () => {
-  test('Returns the same initial times', () => {
-    render(<BookingForm availableTimes={['17:00']} />);
-
-    const timePicker = screen.getByLabelText('Choose time');
-    const times = timePicker.childElementCount;
+  test('Returns the updated times after a date is chosen', () => {
+    let times = [];
+    const mockDispatch = jest.fn(() => times.push('17:00'));
+    render(<BookingForm availableTimes={times} dispatch={mockDispatch} />);
 
     const datePicker = screen.getByLabelText('Choose a date');
-    fireEvent.change(datePicker, { target: { value: 'yyyy-mm-dd'} });
-    const updatedTimes = timePicker.childElementCount;
+    const today = new Date();
+    const value = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    fireEvent.change(datePicker, { target: { value: value } });
 
-    expect(times).toEqual(updatedTimes);
+    const timePicker = screen.getByLabelText('Choose a time');
+    expect(timePicker).toBeEnabled();
+    expect(timePicker.childElementCount).toBeGreaterThan(1);
   });
 });
