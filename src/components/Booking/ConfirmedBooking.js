0d969dcc6db42624e13cc2d8f992405cd981b22f
Joshua Nobleza <joshnobleza@bell.net>
2023-11-21 20:55:47 -0500

    Set up the booking confirmation page

---
+++ src/components/Booking/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+import '../../styles/ConfirmedBooking.scss';
+
+function ConfirmedBooking() {
+    return (
+        <div className='message-container'>
+            <p>Thanks for making a reservation!</p>
+        </div>
+    );
+}
+
+export default ConfirmedBooking;
