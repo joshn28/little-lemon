9cea26a860b30724512fa6ef60190a2d1abcc9a3
Joshua Nobleza <joshnobleza@bell.net>
2023-11-22 20:57:22 -0500

    Make the header logo a link and update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header>
-            <img src={logo} alt="restaurant logo" />
+            <Link to='/'><img src={logo} alt="restaurant logo" /></Link>
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Menu</a></li>
+                    <li>About</li>
+                    <li>Menu</li>
                     <li><Link to='/reserve-a-table'>Reservations</Link></li>
-                    <li><a href="#">Order Online</a></li>
-                    <li><a href="#">Login</a></li>
+                    <li>Order Online</li>
+                    <li>Login</li>
                 </ul>
             </nav>
         </header>
