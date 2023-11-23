f1c681d2ef511a95038bd9a53a8c16d2e7a89122
Joshua Nobleza <joshnobleza@bell.net>
2023-11-22 20:45:59 -0500

    Update validation tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,31 +2,36 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import BookingForm from './components/Booking/BookingForm';
 
 test('Renders the BookingForm heading', () => {
-    render(<BookingForm availableTimes={[]} />);
+  render(<BookingForm availableTimes={[]} />);
 
-    const headingElement = screen.getByText("Reserve a table");
-    expect(headingElement).toBeInTheDocument();
+  const headingElement = screen.getByText("Reserve a table");
+  expect(headingElement).toBeInTheDocument();
 });
 
-test('Check if the time select and submit button are disabled after invalid data', () => {
-  render(<BookingForm availableTimes={[]} />);
+test('Check if the time select and submit button are disabled if data is invalid', () => {
+  let times = [];
+  const mockDispatch = jest.fn(() => times.push('17:00'));
+  render(<BookingForm availableTimes={times} dispatch={mockDispatch} />);
 
   const timePicker = screen.getByLabelText('Choose a time');
   expect(timePicker).toBeDisabled();
 
   const submitButton = screen.getByText('Make your reservation').closest('button');
   expect(submitButton).toBeDisabled();
+
+  const datePicker = screen.getByLabelText('Choose a date');
+  fireEvent.change(datePicker, { target: { value: '2023-11-22' } });
+
+  expect(submitButton).toBeDisabled();
 });
 
-test('Check if the time select and submit button are enabled after valid data', () => {
+test('Check if the time select and submit button are enabled if data is valid', () => {
   let times = [];
   const mockDispatch = jest.fn(() => times.push('17:00'));
   render(<BookingForm availableTimes={times} dispatch={mockDispatch} />);
 
   const datePicker = screen.getByLabelText('Choose a date');
-  const today = new Date();
-  const value = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-  fireEvent.change(datePicker, { target: { value: value } });
+  fireEvent.change(datePicker, { target: { value: '2023-11-22' } });
 
   const timePicker = screen.getByLabelText('Choose a time');
   fireEvent.change(timePicker, { target: { value: '17:00' } });
@@ -54,9 +59,7 @@ describe('updateTimes', () => {
     render(<BookingForm availableTimes={times} dispatch={mockDispatch} />);
 
     const datePicker = screen.getByLabelText('Choose a date');
-    const today = new Date();
-    const value = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-    fireEvent.change(datePicker, { target: { value: value } });
+    fireEvent.change(datePicker, { target: { value: '2023-11-22' } });
 
     const timePicker = screen.getByLabelText('Choose a time');
     expect(timePicker.childElementCount).toBeGreaterThan(1);
