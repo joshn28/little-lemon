c5b39783b5f5b5d44c3cdbd663002cb71f030613
Joshua Nobleza <joshnobleza@bell.net>
2024-01-11 16:35:17 -0500

    Make components responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 import logo from "../assets/Logo.svg";
+import menuIcon from "../assets/hamburger menu.svg";
 import { Link } from "react-router-dom";
 
 function Header() {
+    const [visible, setVisible] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 1023;
+
+    useEffect(() => {
+        window.addEventListener("resize", () => setWidth(window.innerWidth));
+    }, []);
+
     return (
         <header>
             <Link to='/'><img src={logo} alt="restaurant logo" /></Link>
-            <nav>
+            <img src={menuIcon} alt="" className="menu" onClick={() => setVisible(!visible)}/>
+            <nav style={{ visibility: visible || width > breakpoint ? "visible" : "hidden" }}>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
                     <li>About</li>
